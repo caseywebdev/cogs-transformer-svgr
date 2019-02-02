3492185f7748602d7fff6d8aa123dd0736caa820
Casey Foster <c@sey.me>
2019-02-01 20:18:44 -0600

    Update svgr, 2.0.0

--- test/output.js
+++ test/output.js
@@ -1,9 +1,5 @@
 import React from "react";
 
-const Input = props => (
-  <svg {...props}>
-    <path d="M10 10h100v100H10z" />
-  </svg>
-);
+const SvgInput = props => <svg {...props}><path d="M10 10h100v100H10z" /></svg>;
 
-export default Input;
+export default SvgInput;
