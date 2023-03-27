104faf568d81022f91594d2ce5a1b9d04191660a
Casey Foster <c@sey.me>
2023-03-26 21:32:09 -0500

    Fix test

--- test/test.js
+++ test/test.js
@@ -2,7 +2,7 @@ import helper from 'cogs-test-helper';
 
 export default helper.createTests({
   'test/config.js': {
-    'test/input.svg': helper.getFileBuffer('test/output.js'),
+    'test/input.svg': 'test/output.js',
     'test/error.svg': Error
   }
 });
